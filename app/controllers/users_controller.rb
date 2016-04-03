168712bf194aecf91ef4340e81b2d63a807525b9
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-03 17:15:59 -0500

    Full crud for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 
 class UsersController < ApplicationController
   def index
-    #user_list
     @users = User.page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_name: params[:user][:user_name],
+                        first_name: params[:user][:first_name],
+                        last_name: params[:user][:last_name],
+                        email: params[:user][:last_name])
+    redirect_to user_path(@user.id)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to :users
   end
 end
